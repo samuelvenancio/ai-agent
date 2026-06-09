0dd10ce6b68a8d9067ab69d85764b8dcaf4a0624
samuelvenancio <sassatricolado@outlook.com>
2026-06-09 14:55:26 -0300

    LLM Calling

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from google import genai
 from google.genai import types
 
 from prompt import system_prompt
+from call_function import available_functions
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="AI Code Assistant")
@@ -35,8 +36,8 @@ def generate_content(
         model="gemini-2.5-flash",
         contents=messages,
         config=types.GenerateContentConfig(
+            tools=[available_functions],
             system_instruction=system_prompt,
-            temperature=0
         )
     )
     if not response.usage_metadata:
@@ -45,9 +46,14 @@ def generate_content(
     if verbose:
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
         print("Response tokens:", response.usage_metadata.candidates_token_count)
-    print("Response:")
-    print(response.text)
 
+    if not response.function_calls:
+        print("Response:")
+        print(response.text)
+        return
+
+    for function_call in response.function_calls:
+        print(f"Calling function: {function_call.name}({function_call.args})")
 
 if __name__ == "__main__":
     main()
