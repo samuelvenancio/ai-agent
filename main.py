337f94790ebefb2a083ddfd9685a38453fd098af
samuelvenancio <sassatricolado@outlook.com>
2026-06-10 11:09:22 -0300

    AI can call functions

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from google import genai
 from google.genai import types
 
 from prompt import system_prompt
-from call_function import available_functions
+from call_function import available_functions, call_function
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="AI Code Assistant")
@@ -52,8 +52,18 @@ def generate_content(
         print(response.text)
         return
 
+    function_responses: list[types.Part] = []
     for function_call in response.function_calls:
-        print(f"Calling function: {function_call.name}({function_call.args})")
+        result = call_function(function_call, verbose)
+        if (
+            not result.parts
+            or not result.parts[0].function_response
+            or not result.parts[0].function_response.response
+        ):
+            raise RuntimeError(f"Empty function response for {function_call.name}")
+        if verbose:
+            print(f"-> {result.parts[0].function_response.response}")
+        function_responses.append(result.parts[0])
 
 if __name__ == "__main__":
     main()
